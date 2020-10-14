8898c8143ea5bdb9623cae5eedb010a2340c5b59
kkamara <kelvinkamara@protonmail.com>
2020-10-14 18:29:05 +0100

    Use named args

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--bar', help='Do the bar option')
+parser.add_argument('--foo', help='Foo the program')
+
+args = parser.parse_args()
 
 def run():
-    print(sys.argv)
+    print(args)
 
 if __name__ == '__main__':
     run()
